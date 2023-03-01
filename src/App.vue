83232652b99372d37fe4815a5260c3315bbe0ac7
Administrator <zhangshihaovv@163.com>
2023-03-01 18:25:30 +0800

    注册登录页面开发

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import MainView from "@/views/mainView.vue";
+import Login from "@/views/login.vue"
+
 </script>
 
 <template>
-  <MainView></MainView>
+   <router-view></router-view>
 </template>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,45 +5,68 @@ import SettingBlog from "../views/BlogManagement/SettingBlog.vue"
 import BookManagement from '../views/BookManagement/BookManagement.vue'
 import AddBook from '../views/BookManagement/components/AddBook.vue'
 import Home from '../views/Home/Home.vue'
+import MainView from '../views/mainView.vue'
 import CategoryManagement from '../views/CategoryManagement/CategoryManagement.vue'
 import PDFBookManagement from  '../views/PDFBookManagement/PDFBookManagement.vue'
+import Login from '../views/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/home',
-      component:Home
+      path:'/',
+      // redirect:'/home',
+      component:MainView,
+      children:[
+        {
+          path:'blog',
+          component:BlogManagement
+        },
+        {
+          path:'blog/addBlog',
+          component:AddBlog
+        },
+        {
+          path:'blog/settingBlog',
+          component:SettingBlog
+        },
+        {
+          path:'books',
+          component:BookManagement
+        },
+        {
+          path:'pdfbooks',
+          component:PDFBookManagement
+        },
+        {
+          path:'books/addBook',
+          component:AddBook
+        },
+        {
+          path:'category',
+          component:CategoryManagement
+        }
+      ]
     },
     {
-      path:'/blog',
-      component:BlogManagement
+      path:'/login',
+      name:'login',
+      component:Login
     },
-    {
-      path:'/blog/addBlog',
-      component:AddBlog
-    },
-    {
-      path:'/blog/settingBlog',
-      component:SettingBlog
-    },
-    {
-      path:'/books',
-      component:BookManagement
-    },
-    {
-      path:'/pdfbooks',
-      component:PDFBookManagement
-    },
-    {
-      path:'/books/addBook',
-      component:AddBook
-    },
-    {
-      path:'/category',
-      component:CategoryManagement
-    }
   ]
+})
+
+router.beforeEach((to,from)=>{
+  if (to.name!=='login'){
+    const token=window.localStorage.getItem('access_token')
+    if (token===null){
+      return {
+        name:'login'
+      }
+    }
+  }else{
+    return true
+  }
 })
 
 export default router
